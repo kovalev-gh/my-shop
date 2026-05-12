68fecdec7c58a032b6c4065ac7a538069c9b792d
Sergey Kovalev <kovalev.connect@gmail.com>
2026-05-12 16:26:08 +0300

    del calc and hello index endpoints

--- app/main.py
+++ app/main.py
@@ -20,27 +20,10 @@ app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 app.include_router(items_router)
 app.include_router(users_router)
 
-
-@app.get("/")
-def hello_index():
-    return {
-        "message": "Hello index!",
-    }
-
-
 @app.get("/hello/")
 def hello(name: str = "World"):
     name = name.strip().title()
     return {"message": f"Hello {name}!"}
-
-
-@app.get("/calc/add/")
-def add(a: int, b: int):
-    return {
-        "a": a,
-        "b": b,
-        "result": a + b,
-    }
 
 
 if __name__ == "__main__":
