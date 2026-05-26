338697cf8fb929560395471db9a36c0199cc4f9d
Sergey Kovalev <kovalev.connect@gmail.com>
2026-05-27 00:00:32 +0300

    move models to domain architecture

--- app/core/db/models.py
+++ app/core/db/models.py
@@ -1,14 +1,3 @@
-
-
-
 from domains.products.models import Product
 from domains.users.models import User
 from domains.orders.models import Order, OrderItem
-
-
-__all__ = (
-    "Product",
-    "User",
-    "Order",
-    "OrderItem",
-)
